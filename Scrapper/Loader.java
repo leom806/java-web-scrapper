332fd3689a9ac868403abd1bebbd3d3da8aca723
Leonardo Momente <leom806@gmail.com>
2016-12-20 10:20:40 -0200

    Update Loader.java

--- Scrapper/Loader.java
+++ Scrapper/Loader.java
@@ -8,33 +8,40 @@ import java.io.*;
 * @author Leonardo Momente
 * @version 1.0.5.0
 * Data: 09-08-2016
-* Atualizado: 15-12-2016
-*
+* Atualizado: 20-12-2016
+* Descrição: Interface de carregamento, lista os métodos sobrecarregados que 
+* fazem a conversão e também o método de liberação dos recursos que foram alocados.
 */
 
 interface Loader {
-  /**
-  * Opções de chamada dos métodos.
-  *
-  *   status:
-  *   exibe passo a passo do processo de raspagem
-  *   
-  *   display:
-  *   exibe o conteúdo raspado
-  *   
-  *   default:
-  *   será retornado String com conteudo raspado
-  *
-  */
-  String ParseCode(String toFind, File path, boolean status, boolean display);
-  String ParseCode(String toFind, String source, boolean status, boolean display);
-  String ParseCode(boolean status, String toFind, File path);
-  String ParseCode(boolean status, String toFind, String source);
-  String ParseCode(String toFind, File path, boolean display);
-  String ParseCode(String toFind, String source, boolean display);
-  String ParseCode(String toFind, File path);
-  String ParseCode(String toFind, String source);
+    
+    /* 
+    * Variável que armazena o conteúdo final da raspagem, usada no 
+    * retorno dos métodos.
+    */
+    String content = "";
+    
+    /**
+    * Opções de chamada dos métodos.
+    *
+    *   status:
+    *   exibe passo a passo do processo de raspagem
+    *   
+    *   display:
+    *   exibe o conteúdo raspado
+    *   
+    *   default:
+    *   será retornado String com conteudo raspado
+    *
+    */
+    String ParseCode(String toFind, File path, boolean status, boolean display);
+    String ParseCode(String toFind, String source, boolean status, boolean display);
+    String ParseCode(boolean status, String toFind, File path);
+    String ParseCode(boolean status, String toFind, String source);
+    String ParseCode(String toFind, File path, boolean display);
+    String ParseCode(String toFind, String source, boolean display);
+    String ParseCode(String toFind, File path);
+    String ParseCode(String toFind, String source);
 
-  int Parsing(boolean status, boolean display);
-  int Close();
+    int Close();
 } 
