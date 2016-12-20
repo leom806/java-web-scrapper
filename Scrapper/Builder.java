97df0ebad0869aa8a417a81c73fecc4e289b3611
Leonardo Momente <leom806@gmail.com>
2016-12-20 10:21:21 -0200

    Update Builder.java

--- Scrapper/Builder.java
+++ Scrapper/Builder.java
@@ -8,280 +8,286 @@ import java.io.*;
 * @author Leonardo Momente
 * @version 1.0.5.0
 * Data: 15-12-2016
-* Atualizado: 15-12-2016
+* Atualizado: 20-12-2016
 *
-*/
-
+ */
 public class Builder extends Parser implements Loader {
 
-	/**
-	* 'Desacopla' os objetos ; Limpa as variáveis ; etc
-	*/
-  private void Detach() {
-  	//
-  	//
-  	//
-  	//
-  	//
-  	//
-  	//
-  }
-
-  /**
-	* Fecha o objeto e gera retorno
-	*/
-  @Override
-  public int Close() {
-    try { 
-      Detach();
-      return 0;
-    }catch(Exception ex) {
-      System.out.println("Exception: "+ex);
-    }
-    return -1;
-  }
-
-  /**
-  * Métodos Principais Sobrecarregados e Sobrescritos
-  */
-
-	/**
-	 * Método sobrecarregado para busca.
-	 * @param palavra buscada
-	 * @param arquivo fonte
-	 * @param flag de status da busca
-	 * @param flag de display de resultados
-	 * @since 1.0
-	 * @version 1.0
-	 */
-  @Override
-  public String ParseCode(String toFind, File path, boolean status, boolean display) {
-
-    Detach();
-
-    this.toFind = toFind;
-
-    try {
-      bf = new BufferedReader(new FileReader(path));
-    }catch(FileNotFoundException e){
-      System.out.println("Arquivo não encontrado\nErro: "+e);
+    private String toFind = null;
+    
+    protected StringBuilder html = new StringBuilder();
+    protected BufferedReader bf;
+    
+    
+    /**
+     * 'Desacopla' os objetos ; Limpa as variáveis ; etc
+     */
+    private void Detach() {
+        //
+        //
+        //
+        //
+        //
+        //
+        //
     }
 
-    try {
-      html += bf.readLine();
-      while (bf.readLine() != null)
-      {
-        html += bf.readLine();
-      }
-
-    }catch(IOException e){
-      System.out.println("Erro de IO\nErro: "+e);
+    /**
+     * Fecha o objeto e gera retorno
+     */
+    @Override
+    public int Close() {
+        try {
+            Detach();
+            return 0;
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex);
+        }
+        return -1;
     }
 
-    try {
-		bf.close();
-	   } catch (IOException e) {
-		e.printStackTrace();
-	   }
+    /**
+     * Métodos Principais Sobrecarregados e Sobrescritos
+     */
+    /**
+     * Método sobrecarregado para busca.
+     *
+     * @param palavra buscada
+     * @param arquivo fonte
+     * @param flag de status da busca
+     * @param flag de display de resultados
+     * @since 1.0
+     * @version 1.0
+     */
+    @Override
+    public String ParseCode(String toFind, File path, boolean status, boolean display) {
 
-    Parsing(status, display);
+        Detach();
 
-    return content;
-  }
+        this.toFind = toFind;
 
+        try {
+            bf = new BufferedReader(new FileReader(path));
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado\nErro: " + e);
+        }
 
-  /**
-   * Método sobrecarregado para busca.
-   * @param palavra buscada
-   * @param String fonte
-   * @param flag de status da busca
-   * @param flag de display de resultados
-   * @since 1.0
-   * @version 1.0
-   */
-  @Override
-  public String ParseCode(String toFind, String source, boolean status, boolean display) {
+        try {
+            html.append(bf.readLine());
+            while (bf.readLine() != null) {
+                html.append(bf.readLine());
+            }
 
-    Detach();
-    this.toFind = toFind;
-    this.html = source;
+        } catch (IOException e) {
+            System.out.println("Erro de IO\nErro: " + e);
+        }
 
-    Parsing(status, display);
+        try {
+            bf.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-    return content;
-  }
+        Parsing(status, display);
 
-  /**
-   * Método sobrecarregado para busca.
-   * @param flag de status da busca
-   * @param palavra buscada
-   * @param arquivo fonte
-   * @since 1.0
-   * @version 1.0
-   */
-  @Override
-  public String ParseCode(boolean status, String toFind, File path) {
-
-    Detach();
-
-    this.toFind = toFind;
-
-    try {
-      bf = new BufferedReader(new FileReader(path));
-    }catch(FileNotFoundException e){
-      System.out.println("Arquivo não encontrado\nErro: "+e);
+        return content;
     }
 
-    try {
-      html += bf.readLine();
-      while (bf.readLine() != null)
-      {
-        html += bf.readLine();
-      }
+    /**
+     * Método sobrecarregado para busca.
+     *
+     * @param palavra buscada
+     * @param String fonte
+     * @param flag de status da busca
+     * @param flag de display de resultados
+     * @since 1.0
+     * @version 1.0
+     */
+    @Override
+    public String ParseCode(String toFind, String source, boolean status, boolean display) {
 
-      bf.close();
-    }catch(IOException e){
-      System.out.println("Erro de IO\nErro: "+e);
+        Detach();
+        this.toFind = toFind;
+        this.html.append(source);
+
+        Parsing(status, display);
+
+        return content;
     }
 
-    Parsing(status, true);
+    /**
+     * Método sobrecarregado para busca.
+     *
+     * @param flag de status da busca
+     * @param palavra buscada
+     * @param arquivo fonte
+     * @since 1.0
+     * @version 1.0
+     */
+    @Override
+    public String ParseCode(boolean status, String toFind, File path) {
 
-    return content;
-  }
+        Detach();
 
-  /**
-   * Método sobrecarregado para busca.
-   * @param flag de status da busca
-   * @param palavra buscada
-   * @param String fonte
-   * @since 1.0
-   * @version 1.0
-   */
-  @Override
-  public String ParseCode(boolean status, String toFind, String source) {
+        this.toFind = toFind;
 
-    Detach();
-    this.toFind = toFind;
-    this.html = source;
+        try {
+            bf = new BufferedReader(new FileReader(path));
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado\nErro: " + e);
+        }
 
-    Parsing(status, true);
+        try {
+            html.append(bf.readLine());
+            while (bf.readLine() != null) {
+                html.append(bf.readLine());
+            }
 
-    return content;
-  }
+            bf.close();
+        } catch (IOException e) {
+            System.out.println("Erro de IO\nErro: " + e);
+        }
 
-  /**
-   * Método sobrecarregado para busca.
-   * @param palavra buscada
-   * @param arquivo fonte
-   * @param flag de display de resultados
-   * @since 1.0
-   * @version 1.0
-   */
-  @Override
-  public String ParseCode(String toFind, File path, boolean display) {
+        Parsing(status, true);
 
-    Detach();
-
-    this.toFind = toFind;
-
-    try {
-      bf = new BufferedReader(new FileReader(path));
-    }catch(FileNotFoundException e){
-      System.out.println("Arquivo não encontrado\nErro: "+e);
+        return content;
     }
 
-    try {
-      html += bf.readLine();
-      while (bf.readLine() != null)
-      {
-        html += bf.readLine();
-      }
+    /**
+     * Método sobrecarregado para busca.
+     *
+     * @param flag de status da busca
+     * @param palavra buscada
+     * @param String fonte
+     * @since 1.0
+     * @version 1.0
+     */
+    @Override
+    public String ParseCode(boolean status, String toFind, String source) {
 
-      bf.close();
-    }catch(IOException e){
-      System.out.println("Erro de IO\nErro: "+e);
+        Detach();
+        this.toFind = toFind;
+        this.html.append(source);
+
+        Parsing(status, true);
+
+        return content;
     }
 
-    Parsing(false, display);
+    /**
+     * Método sobrecarregado para busca.
+     *
+     * @param palavra buscada
+     * @param arquivo fonte
+     * @param flag de display de resultados
+     * @since 1.0
+     * @version 1.0
+     */
+    @Override
+    public String ParseCode(String toFind, File path, boolean display) {
 
-    return content;
-  }
+        Detach();
 
-  /**
-   * Método sobrecarregado para busca.
-   * @param palavra buscada
-   * @param String fonte
-   * @param flag de display de resultados
-   * @since 1.0
-   * @version 1.0
-   */
-  @Override
-  public String ParseCode(String toFind, String source, boolean display) {
+        this.toFind = toFind;
 
-    Detach();
-    this.toFind = toFind;
-    this.html = source;
+        try {
+            bf = new BufferedReader(new FileReader(path));
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado\nErro: " + e);
+        }
 
-    Parsing(false, display);
+        try {
+            html.append(bf.readLine());
+            while (bf.readLine() != null) {
+                html.append(bf.readLine());
+            }
 
-    return content;
-  }
+            bf.close();
+        } catch (IOException e) {
+            System.out.println("Erro de IO\nErro: " + e);
+        }
 
-  /**
-   * Método sobrecarregado para busca.
-   * @param palavra buscada
-   * @param arquivo fonte
-   * @since 1.0
-   * @version 1.0
-   */
-  @Override
-  public String ParseCode(String toFind, File path) {
+        Parsing(false, display);
 
-    Detach();
-
-    this.toFind = toFind;
-
-    try {
-      bf = new BufferedReader(new FileReader(path));
-    }catch(FileNotFoundException e){
-      System.out.println("Arquivo não encontrado\nErro: "+e);
+        return content;
     }
 
-    try {
-      html += bf.readLine();
-      while (bf.readLine() != null)
-      {
-        html += bf.readLine();
-      }
+    /**
+     * Método sobrecarregado para busca.
+     *
+     * @param palavra buscada
+     * @param String fonte
+     * @param flag de display de resultados
+     * @since 1.0
+     * @version 1.0
+     */
+    @Override
+    public String ParseCode(String toFind, String source, boolean display) {
 
-      bf.close();
-    }catch(IOException e){
-      System.out.println("Erro de IO\nErro: "+e);
+        Detach();
+        this.toFind = toFind;
+        this.html.append(source);
+
+        Parsing(false, display);
+
+        return content;
     }
 
-    Parsing(false, false);
+    /**
+     * Método sobrecarregado para busca.
+     *
+     * @param palavra buscada
+     * @param arquivo fonte
+     * @since 1.0
+     * @version 1.0
+     */
+    @Override
+    public String ParseCode(String toFind, File path) {
 
-    return content;
-  }
+        Detach();
 
-	/**
-   * Método sobrecarregado para busca.
-   * @param palavra buscada
-   * @param String fonte
-   * @since 1.0
-   * @version 1.0
-   */
-  @Override
-  public String ParseCode(String toFind, String source) {
+        this.toFind = toFind;
 
-    Detach();
-    this.toFind = toFind;
-    this.html = source;
+        try {
+            bf = new BufferedReader(new FileReader(path));
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado\nErro: " + e);
+        }
 
-    Parsing(false, false);
+        try {
+            html.append(bf.readLine());
+            while (bf.readLine() != null) {
+                html.append(bf.readLine());
+            }
 
-    return content;
-  }
+            bf.close();
+        } catch (IOException e) {
+            System.out.println("Erro de IO\nErro: " + e);
+        }
 
+        Parsing(false, false);
+
+        return content;
+    }
+
+    /**
+     * Método sobrecarregado para busca.
+     *
+     * @param palavra buscada
+     * @param String fonte
+     * @since 1.0
+     * @version 1.0
+     */
+    @Override
+    public String ParseCode(String toFind, String source) {
+
+        Detach();
+        this.toFind = toFind;
+        this.html.append(source);
+
+        Parsing(false, false);
+
+        return content;
+    }
 
 }
