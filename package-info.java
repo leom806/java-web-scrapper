74ea6152c3068fd03b79debcadf1e6a9659ffe1e
Leonardo Momente <leom806@gmail.com>
2016-12-24 21:00:12 -0200

    Add files via upload

---
+++ package-info.java
@@ -0,0 +1,20 @@
+/**
+*
+* Content-Search Algorithm
+* 
+* @author Leonardo Momente
+* @version 1.0.5.2
+* 
+* LIBRARY(S): 
+*   > Jsoup 1.10.1;
+* 
+* Last Update: 24-12-2016
+* DESCRIPTION: Parser is the main object here. It extends the Builder and implements
+* the Loader interface. Builder is just a secondary class to implement some methods.
+* Main class is to call Parser.
+* 
+* These classes makes up the software as it is (GPL 3.0).
+* 
+*/
+
+package org.scrapper;
