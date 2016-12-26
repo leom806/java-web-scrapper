4a4195585ddaa38e9132cc7f5f481337d9501515
Leonardo Momente <leom806@gmail.com>
2016-12-26 03:54:48 -0200

    Add files via upload

--- package-info.java
+++ package-info.java
@@ -3,12 +3,14 @@
 * Content-Search Algorithm
 * 
 * @author Leonardo Momente
-* @version 1.0.5.2
+* @version 1.0.5.3
+* 
+* SOTWARE STATUS: working.
 * 
 * LIBRARY(S): 
 *   > Jsoup 1.10.1;
 * 
-* Last Update: 24-12-2016
+* LAST UPDATE: 26-12-2016
 * DESCRIPTION: Parser is the main object here. It extends the Builder and implements
 * the Loader interface. Builder is just a secondary class to implement some methods.
 * Main class is to call Parser.
