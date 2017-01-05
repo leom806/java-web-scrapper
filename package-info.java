b456c85058588ac7527aff95dd3f8dbd08215912
Leonardo Momente <leom806@gmail.com>
2017-01-05 12:51:09 -0300

    Update package-info.java

--- package-info.java
+++ package-info.java
@@ -3,17 +3,19 @@
 * Content-Search Algorithm
 * 
 * @author Leonardo Momente
-* @version 1.0.5.4
+* @version 1.0.5.5
 * 
 * SOTWARE STATUS: working.
 * 
 * LIBRARY(S): 
 *   > Jsoup 1.10.1;
 * 
-* LAST UPDATE: 26-12-2016
-* DESCRIPTION: Parser is the main object here. It extends the GUIStream and implements
-* the Loader interface. Builder is just a secondary class to implement some methods.
-* Main class is to call Parser using the GUI class.
+* LAST UPDATE: 05-01-2017
+* DESCRIPTION: Parser is the main object here. It extends the Builder and implements
+* the Loader interface. Builder is just a secondary class to implement auxiliar methods.
+* Main class is to bootstrap Parser using the GUI class.
+*
+* WikipediaParser is an webpage setup Parser class.
 * 
 * These classes makes up the software as it is (GPL 3.0).
 * 
